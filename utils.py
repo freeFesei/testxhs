8a75d89e2d1a8758778cb6cb2fa259c734047aac
freeFesei <1124011648@qq.com>
2025-07-06 21:07:37 +0800

    bug修改

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from prompt_template import system_template_text, user_template_text
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_deepseek import ChatDeepSeek
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.output_parsers import JsonOutputParser
 from langchain.prompts import ChatPromptTemplate
 from xiaohongshu_model import Xiaohongshu
 
@@ -9,17 +9,28 @@ from xiaohongshu_model import Xiaohongshu
 
 
 def generate_xiaohongshu(theme, openai_api_key):
+
+    # model = ChatOpenAI(model="gpt-3.5-turbo", api_key=openai_api_key)
+    model = ChatDeepSeek(model="deepseek-chat", api_key=openai_api_key,api_base='https://api.deepseek.com')
+    output_parser = JsonOutputParser(pydantic_object=Xiaohongshu)
+
     prompt = ChatPromptTemplate.from_messages([
         ("system", system_template_text),
         ("user", user_template_text)
     ])
-    model = ChatDeepSeek(model="deepseek-reasoner", api_key=openai_api_key)
-    output_parser = PydanticOutputParser(pydantic_object=Xiaohongshu)
+
+
     chain = prompt | model | output_parser
+
+    print(output_parser.get_format_instructions())
     result = chain.invoke({
         "parser_instructions": output_parser.get_format_instructions(),
         "theme": theme
     })
+    print(type(result))
+
+    print(result)
+    print(result['titles'])
     return result
 
 # print(generate_xiaohongshu("大模型", os.getenv("OPENAI_API_KEY")))
--- xiaohongshu_model.py
+++ xiaohongshu_model.py
@@ -1,6 +1,10 @@
-from langchain_core.pydantic_v1 import BaseModel, Field
+# from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
+
 from typing import List
 
 class Xiaohongshu(BaseModel):
-    titles: List[str] = Field(description="小红书的5个标题", min_items=5, max_items=5)
+    titles: List[str] = Field(description="小红书的5个标题")
     content: str = Field(description="小红书的正文内容")
+
+
